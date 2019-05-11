4f5b09e2cd53c55d17484210eddda76fdf37db86
mikey <manicdesigner@gmail.com>
2019-05-11 12:32:05 -0400

    feat(rm activeTab) remove active tab took active-tab class off all tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,12 +21,13 @@ class TabLink {
   }
 
   selectTab() {
-    console.log('grab');
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
 
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(tab => {
+      tab.classList.remove('active-tab');
+    });
 
     // Select all of the elements with the .card class on them
     // const cards = ;
